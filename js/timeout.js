8a2c3c8e69009e89263c94692f2ba7a683cb1b61
Donnie Plumly <donnie.plumly@gmail.com>
2022-11-02 10:07:30 -0700

    first

---
+++ js/timeout.js
@@ -0,0 +1,14 @@
+let time = new Date().getTime();
+const setActivityTime = (e) => {
+    time = new Date().getTime();
+}
+document.body.addEventListener("mousemove", setActivityTime);
+document.body.addEventListener("keypress", setActivityTime);
+const refresh = () => {
+    if (new Date().getTime() - time >= 60000) {
+        window.location.reload(true);
+    } else {
+        setTimeout(refresh, 10000);
+    }
+}
+setTimeout(refresh, 10000);
